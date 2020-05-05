e8b793d2636721234e5be90ce2423ff362228439
Victoris <52378018+sviridchik@users.noreply.github.com>
2020-05-05 15:50:42 +0300

    Add files via upload

--- Neyro.cs
+++ Neyro.cs
@@ -9,14 +9,39 @@ namespace firsttry
 {
     class Neyro
     {
-        public List<Matrica<double>> vesa;
+       // public List<Matrica<double>> vesa;
         public Matrica<double> output;
-        public Matrica<double> bios;
+        public Matrica<double> bias;
         int inputSize;
         public Random r;
+
+
+
+        public Neyro() {
+            for (int i = 0; i < 724; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    output[i, j] = r.Next(-1, 1);
+                   // Console.Write("{0}\t", output[i, j]);
+                }
+                //Console.WriteLine();
+            }
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    bias[i, j] = r.Next(-1, 1);
+                    // Console.Write("{0}\t", output[i, j]);
+                }
+                //Console.WriteLine();
+            }
+        }
         public Matrica<double> Predict(Matrica<double> input)
         {
-            return (input *= output)+bios; 
+            return Softmax(input *= output)+bias); 
         }
         public Matrica<double> Softmax(Matrica<double> input)
         {
@@ -28,7 +53,7 @@ namespace firsttry
                 for (int j = 0; j < input.Cells; j++)
                 {
                    
-                        dynamic a = input[i, j];
+                   dynamic a = input[i, j];
                    denominator += Math.Exp(a);
                     
                 }
